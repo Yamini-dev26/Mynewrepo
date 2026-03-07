0b964c2cef930829a2e32d91080b84d731523d9b
Yamini-dev26 <kakarlapriyanka03@gmail.com>
2026-03-07 15:32:01 -0600

    New code is added

--- app.java
+++ app.java
@@ -3,3 +3,28 @@ public class Main {
     System.out.println("Hello World");
   }
 }
+
+public class SwapNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.20f, second = 2.45f;
+
+        System.out.println("--Before swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+
+        // Value of first is assigned to temporary
+        float temporary = first;
+
+        // Value of second is assigned to first
+        first = second;
+
+        // Value of temporary (which contains the initial value of first) is assigned to second
+        second = temporary;
+
+        System.out.println("--After swap--");
+        System.out.println("First number = " + first);
+        System.out.println("Second number = " + second);
+    }
+}
